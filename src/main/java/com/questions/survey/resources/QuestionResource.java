1e9f6fd2fce988c6bc61d2b52d20ca7ffdeea2f3
Felipe Voigt <felipe.voigt.12@gmail.com>
2020-01-13 09:32:33 -0300

    Adicionando Swagger

--- src/main/java/com/questions/survey/resources/QuestionResource.java
+++ src/main/java/com/questions/survey/resources/QuestionResource.java
@@ -3,6 +3,7 @@ package com.questions.survey.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,34 +16,45 @@ import org.springframework.web.bind.annotation.RestController;
 import com.questions.survey.models.Question;
 import com.questions.survey.repository.QuestionRepository;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @RequestMapping(value="/api")
+@Api(value="API REST Survey")
+@CrossOrigin(origins="*")
 public class QuestionResource {
 
 	@Autowired
 	QuestionRepository questionRepository;
 	
+	
 	@GetMapping("/question")
+	@ApiOperation(value="Retorna uma lista de questões")
 	public List<Question> listQuestion() {
 		return questionRepository.findAll();
 	}
 	
 	@GetMapping("/question/{id}")
+	@ApiOperation(value="Retorna uma única questão")
 	public Question listQuestionById(@PathVariable(value="id") long id) {
 		return questionRepository.findById(id);
 	}
 	
 	@PostMapping("/question")
+	@ApiOperation(value="Cadastra uma questão")
 	public Question saveQuestion(@RequestBody Question question) {
 		return questionRepository.save(question);
 	}
 	
 	@DeleteMapping("/question")
+	@ApiOperation(value="Delete uma questão")
 	public void deleteQuestion(@RequestBody Question question) {
 		questionRepository.delete(question);
 	}
 	
 	@PutMapping("/question")
+	@ApiOperation(value="Atualiza uma questão")
 	public void updateQuestion(@RequestBody Question question) {
 		questionRepository.save(question);
 	}
